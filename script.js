657efa767dd1fbb79ba62107cf7cd2df9b66fac0
wannacry3030 <wannacry.bat@proton.me>
2023-04-03 19:14:53 -0300

    att

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 'use strict';
 
+//trabalhando com classes
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-btnCloseModal.addEventListener('click', function () {
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
